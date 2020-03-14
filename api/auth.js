44885dfe737d398e2946af07c0e21dde19da5f14
Michael Tanel <michael_tanel@outlook.com>
2020-03-14 19:44:13 -0400

    Added body parser Added docker-compose.yml file

--- api/auth.js
+++ api/auth.js
@@ -3,9 +3,7 @@ import express from 'express';
 let router = express.Router();
 
 router.put('/register', (req, res) => {
-    console.log('req body', req.body);
     // If any of these are not set, fail registration.
-    // TODO: Install body parser
     if (!req.body.email || !req.body.pass || !req.body.confirmPass) {
         res.send({
             message: 'Either the email, or password fields were not set correctly',
--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import auth from './api/auth';
+import bodyParser from 'body-parser';
 
 // Setup express app
 const app = express();
 
 const PORT = 8080;
+
+// Used to parse the body for incoming requests.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Enables all endpoints in auth file
 app.use(auth);
